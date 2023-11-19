726918f5d751a40ddbe7513cdf0bacd6a3c39f79
Jadah Pierce <145732309+heyitsmejadah@users.noreply.github.com>
2023-11-19 10:53:47 -0700

    Fixed functionality of the local storage setting.

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 var saveButtons = document.getElementsByClassName("saveBtn");
-console.log(saveButtons);
+// Display current date
+var today = dayjs();
+$('#currentDay').text(today.format('dddd, MMMM D'));
 
-for (var i = 0; i < saveButtons.length; i++) {
+document.addEventListener("DOMContentLoaded", function() {
+  for (var i = 0; i < saveButtons.length; i++) {
+    var savedEventText = localStorage.getItem("eventText" + i);
+    if (savedEventText) {
+    // Update the ".description" element with the saved data
+    var eventTextElement = saveButtons[i].parentNode.querySelector(".description");
+    if (eventTextElement) {
+      eventTextElement.value = JSON.parse(savedEventText);
+    }
+    }
+  }
+});
+
+for (let i = 0; i < saveButtons.length; i++) {
   saveButtons[i].addEventListener("click", function(event) {
     event.preventDefault();
-    var eventText = textarea.value.trim();
-    localStorage.setItem("eventText", JSON.stringify(eventText));
-    renderMessage();
+
+    // Traverse DOM to find the ".description" element
+    var eventText = this.parentNode.querySelector(".description");
+
+    console.log("Clicked save button:", this);
+    console.log("Parent node of save button:", this.parentNode);
+    console.log("Found .description element:", eventText);
+
+    // Check if the ".description" element is found before saving
+    if (eventText) {
+      console.log("Content before saving:", eventText.value);
+      localStorage.setItem("eventText" + i, JSON.stringify(eventText.value));
+      console.log("Content saved:", eventText.value);
+    } else {
+      console.error("Error: '.description' element not found");
+    }
   });
 }
 
-// display the current date in the header of the page.
-var today = dayjs();
-$('#currentDay').text(today.format('dddd, MMMM D'));
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-});
+
